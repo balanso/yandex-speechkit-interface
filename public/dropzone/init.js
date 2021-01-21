cb66f41a7a7abcfab5c49b91900eee7e2c2c1607
balanso <lev1t@yandex.ru>
2021-01-21 19:38:56 +0300

    Fixed js timeout dropzone

--- public/dropzone/init.js
+++ public/dropzone/init.js
@@ -2,6 +2,7 @@ Dropzone.autoDiscover = false
 
 var dropzone = new Dropzone("form#audio-uploader", {
 	url: "/upload",
+	timeout: 0,
 	success: (file, response) => {
 		console.log(file, response)
 	},
